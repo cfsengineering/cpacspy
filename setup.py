91f82acfc9216cb63c38dd1eda5bfe4ced42f445
aidanjungo <aidan@cfse.ch>
2022-06-03 14:18:28 +0200

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import setuptools
 
 NAME = "cpacspy"
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 AUTHOR = "Aidan Jungo"
 EMAIL = "aidan.jungo@cfse.ch"
 DESCRIPTION = "PyPI package to work with CPACS file and AeroMaps"
