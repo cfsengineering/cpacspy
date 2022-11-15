979d2e4e9659a7f2c2af1a74ce6247121fe95f2c
aidanjungo <aidan@cfse.ch>
2022-11-15 09:09:55 +0100

    Bump to version 0.1.5

--- src/cpacspy/__version__.py
+++ src/cpacspy/__version__.py
@@ -1,5 +1,5 @@
 # Changing the version will automatically publish a new version on pypi
 # (see /.github/workflows/pypi.yml)
 
-VERSION = (0, 1, 4)
+VERSION = (0, 1, 5)
 __version__ = ".".join(map(str, VERSION))
