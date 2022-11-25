2ebf7da18901b583b480be00e8049556fd08b947
aidanjungo <aidan@cfse.ch>
2022-11-25 08:54:08 +0100

    Update version

--- src/cpacspy/__version__.py
+++ src/cpacspy/__version__.py
@@ -1,5 +1,5 @@
 # Changing the version will automatically publish a new version on pypi
 # (see /.github/workflows/pypi.yml)
 
-VERSION = (0, 1, 6)
+VERSION = (0, 1, 7)
 __version__ = ".".join(map(str, VERSION))
