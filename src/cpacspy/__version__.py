b2db44acc2574e5d2c83768f4587bcf3963bffd3
GBenedett <113425078+GBenedett@users.noreply.github.com>
2025-04-11 08:48:20 +0200

    Update __version__.py

--- src/cpacspy/__version__.py
+++ src/cpacspy/__version__.py
@@ -1,5 +1,5 @@
 # Changing the version will automatically publish a new version on pypi
 # (see /.github/workflows/pypi.yml)
 
-VERSION = (0, 1, 8)
+VERSION = (0, 2, 0)
 __version__ = ".".join(map(str, VERSION))
